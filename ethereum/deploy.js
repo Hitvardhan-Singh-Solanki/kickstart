281364cbd84cc6f664d83f469ec1134ed9eef6c0
Hitvardhan Singh Solanki <Hitvardhan.Solanki@gds.ey.com>
2019-09-13 01:39:35 +0530

    infura address

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,24 +1,25 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const compiledFactory = require('./build/CampaignFactory.json');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
-  'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
-  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+  "call glow acoustic vintage front ring trade assist shuffle mimic volume reject",
+  // 'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q',
+  "https://rinkeby.infura.io/v3/6d7aff1d5da149d693a51a39d9c47c26"
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(
     JSON.parse(compiledFactory.interface)
   )
     .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: "1000000", from: accounts[0] });
 
-  console.log('Contract deployed to', result.options.address);
+  console.log("Contract deployed to", result.options.address);
 };
 deploy();
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,14 +1,14 @@
-import Web3 from 'web3';
+import Web3 from "web3";
 
 let web3;
 
-if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // We are in the browser and metamask is running.
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+    "https://rinkeby.infura.io/v3/6d7aff1d5da149d693a51a39d9c47c26"
   );
   web3 = new Web3(provider);
 }
